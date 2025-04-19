0842b87130a4375d37957b63627847d44667b36d
Rod Christiansen <rchristiansen@ecuad.ca>
2025-04-19 13:03:38 -0700

    Initial Commit

---
+++ ingraph/ingraphApp.swift
@@ -0,0 +1,17 @@
+//
+//  ingraphApp.swift
+//  ingraph
+//
+//  Created by Rod Christiansen on 2025-04-19.
+//
+
+import SwiftUI
+
+@main
+struct ingraphApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
